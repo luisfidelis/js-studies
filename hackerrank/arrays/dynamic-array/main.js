a010076e4f5e58527e9b3cc480f648095156f408
Luis Philipe Fidelis Fernandes Silva <phil@pop-os.localdomain>
2022-06-16 16:06:36 -0300

    solve dynamic arrays problem

---
+++ hackerrank/arrays/dynamic-array/main.js
@@ -0,0 +1,79 @@
+'use strict';
+
+const fs = require('fs');
+const { serialize } = require('v8');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'dynamicArray' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. 2D_INTEGER_ARRAY queries
+ */
+
+function dynamicArray(n, queries) {
+
+    const arr = Array(n);
+    const answers = [];
+    let lastAnswer = 0;
+    queries.forEach((queryEntry) => {
+        let queryType = queryEntry[0];
+        let x = queryEntry[1];
+        let y = queryEntry[2];
+        if(queryType === 1) {
+            const index = (( x ^ lastAnswer ) % n );
+            if(!arr[index]) arr[index] = [];
+            arr[index].push(y);
+        } else {
+            const index = (( x ^ lastAnswer ) % n );
+            lastAnswer = arr[index][y % arr[index].length];
+            answers.push(lastAnswer);
+        }
+    })
+    return answers;
+    // Write your code here
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+    const n = parseInt(firstMultipleInput[0], 10);
+
+    const q = parseInt(firstMultipleInput[1], 10);
+
+    let queries = Array(q);
+
+    for (let i = 0; i < q; i++) {
+        queries[i] = readLine().replace(/\s+$/g, '').split(' ').map(queriesTemp => parseInt(queriesTemp, 10));
+    }
+
+    const result = dynamicArray(n, queries);
+
+    ws.write(result.join('\n') + '\n');
+
+    ws.end();
+}
