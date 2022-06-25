b967cf6b42cb487b4472f02592d422153d36f604
Luis Philipe Fidelis Fernandes Silva <phil@pop-os.localdomain>
2022-06-24 21:56:06 -0300

    slidding window introduction

---
+++ grokking-the-coding-interview/2-sliding-window/introduction.js
@@ -0,0 +1,41 @@
+/**
+ * Problem: Given an array, find the average of all subarrays of 'K' contiguous elements in it.
+ * 
+ */
+
+
+// --- Bad Solution
+
+function find_averages_bad(k, arr) {
+    const result = [];
+    for(let i=0; i < arr.length - k + 1; i++) {
+        let sum = 0.0;
+        for(let j=i; j < i + k; j++) {
+            sum += arr[j];
+        }
+        result[i] = sum / k;
+    }
+    return result;
+}
+
+
+// --- efficient Solution
+
+function find_averages_eficient(k, arr) {
+    const result = [];
+    let windowSum = 0.0;
+    let windowStart=0;
+    for(let windowEnd=0; windowEnd < arr.length - k + 1; windowEnd++) {
+        windowSum += arr[windowEnd]
+        if(windowEnd >= k - 1) {
+            result.push(windowSum/k);
+            windowSum -= arr[windowStart]; //subtract the element
+            windowStart += 1; // slide window ahead
+        }
+    }
+    return result;
+}
+
+
+[0,1,3,4,5,6,7,8]
+
